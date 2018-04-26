f49bd381e01b6900f2a0556ea661e857d2155820
bill white <wmpwhite@msn.com>
2018-04-26 17:39:40 -0400

    code to establish connection with database working, package.json created

---
+++ bamazonCustomer.js
@@ -0,0 +1,21 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
